854d7a9d4bc97be4bac8e2caf0d637abb6acadf6
Ahmed <ahm3d.ad3l@gmail.com>
2019-03-20 11:09:41 +0200

    added database to assets and kapt dependency to gradle

--- usecases/src/main/java/com/weather/usecases/database/CitiesDao.kt
+++ usecases/src/main/java/com/weather/usecases/database/CitiesDao.kt
@@ -7,7 +7,10 @@ import com.weather.entities.City
 @Dao
 interface CitiesDao {
 
-    @Query("select * from City where City.name like :name")
+    @Query("select * from City ")
+    fun queryAll() : List<City>
+
+    @Query("select * from City where City.name like '%' || :name || '%' ")
     fun queryCitiesByName(name: String) : List<City>
 
     @Query("select * from City where City.id in (:ids)")
---
+++ usecases/src/main/java/com/weather/usecases/database/DatabaseInitializers.kt
@@ -0,0 +1,40 @@
+package com.weather.usecases.database
+
+import android.arch.persistence.room.Room
+import android.content.Context
+import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStream
+
+private const val DATABASE_NAME = "DatabaseGateway.db"
+
+fun initializeDatabase(context: Context): WeatherDatabase {
+    copyDatabaseFileIfNotCreated(context)
+    return buildDatabase(context)
+}
+
+private fun copyDatabaseFileIfNotCreated(context: Context) {
+    context.getDatabasePath(DATABASE_NAME)
+        ?.takeUnless { it.exists() }
+        ?.let { copy(context, it) }
+}
+
+
+private fun copy(context: Context, databaseFile: File) {
+    databaseFile.parentFile.mkdirs()
+    context.assets.open(DATABASE_NAME)
+        .use { copyByteArray(databaseFile, it) }
+}
+
+private fun copyByteArray(databaseFile: File, assetsInputStream: InputStream) {
+    FileOutputStream(databaseFile)
+        .use { it.write(byteArray(assetsInputStream)) }
+}
+
+private fun byteArray(assetsFileInputStream: InputStream) =
+    ByteArray(assetsFileInputStream.available())
+        .also { assetsFileInputStream.read(it) }
+
+
+private fun buildDatabase(context: Context) =
+    Room.databaseBuilder(context, WeatherDatabase::class.java, DATABASE_NAME).build()
