746910b55590ed88ae678186d0718995fd8ddb6f
Ahmed <ahm3d.ad3l@gmail.com>
2019-03-25 21:53:50 +0200

    initializing retrofit

--- usecases/src/main/java/com/weather/domain/ServerGateway.kt
+++ usecases/src/main/java/com/weather/domain/ServerGateway.kt
@@ -1,2 +1,17 @@
 package com.weather.domain
 
+import com.weather.entities.ForecastsResponse
+import io.reactivex.Observable
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+
+private const val SERVER_BASE_URL = "api.openweathermap.org/"
+
+interface ServerApis {
+
+    @GET("data/2.5/forecast")
+    fun requestFiveDaysForecast(@Query("id") cityId: String): Observable<ForecastsResponse>
+
+
+}
