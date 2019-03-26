090a128d956f47701193d14f64ba74aeedb0219f
Ahmed <ahm3d.ad3l@gmail.com>
2019-03-26 22:24:16 +0200

    working with retrofit and MVP

--- app/src/main/java/com/waether/app/features/forecast/ForecasetScreen.kt
+++ app/src/main/java/com/waether/app/features/forecast/ForecasetScreen.kt
@@ -1,8 +1,61 @@
 package com.waether.app.features.forecast
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.waether.app.R
 import com.waether.app.core.ContentViewId
+import com.waether.app.features.home.EXTRA_CITY
+import com.weather.entities.City
+import com.weather.entities.Forecast
+import kotlinx.android.synthetic.main.activity_main.*
 
 @ContentViewId(R.layout.activity_main)
-class ForecastActivity : AppCompatActivity()
+class ForecastActivity : AppCompatActivity(), ForecastView {
+
+    private val presenter: ForecastPresenter by lazy {
+        ForecastPresenterImplementer(this)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val city = intent.getSerializableExtra(EXTRA_CITY) as City
+        presenter.initializeView(city)
+
+
+        add_to_favorites_button.setOnClickListener {
+            if (it.isSelected) {
+                presenter.removeCityFromFavorites()
+            } else {
+                presenter.addCityToFavoritesClicked()
+            }
+        }
+
+    }
+
+    override fun startLoading() {
+//        progress.visibility = VISIBLE
+    }
+
+    override fun stopLoading() {
+//        progress.visibility = GONE
+    }
+
+    override fun drawForcastList(forcasts: List<Forecast>) {
+//        recyclerView.adapter = ForcastAdapter(forcasts)
+    }
+
+    override fun drawCityTitle(cityTitle: String) {
+
+    }
+
+    override fun drawAsFavoriteCity() {
+        add_to_favorites_button.isSelected = true
+    }
+
+    override fun drawAsNotFavoriteCity() {
+        add_to_favorites_button.isSelected = false
+    }
+}
+
+
+
--- usecases/src/main/java/com/weather/domain/ServerGateway.kt
+++ usecases/src/main/java/com/weather/domain/ServerGateway.kt
@@ -1,17 +1,39 @@
 package com.weather.domain
 
 import com.weather.entities.ForecastsResponse
-import io.reactivex.Observable
+import io.reactivex.Single
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 
-private const val SERVER_BASE_URL = "api.openweathermap.org/"
+private const val SERVER_BASE_URL = "http://api.openweathermap.org/"
+private const val APP_ID_KEY = "appid"
+private const val APP_ID_VALUE = "cc8bf0ef9fefd3794a362f69e9b0721d"
 
-interface ServerApis {
+
+private val retrofit: Retrofit by lazy {
+    Retrofit.Builder()
+        .baseUrl(SERVER_BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+        .build()
+}
+
+val forecastsApis: ForecastsApis by lazy {
+    retrofit.create(ForecastsApis::class.java)
+}
+
+
+interface ForecastsApis {
 
     @GET("data/2.5/forecast")
-    fun requestFiveDaysForecast(@Query("id") cityId: String): Observable<ForecastsResponse>
-
-
+    fun requestFiveDaysForecast(
+        @Query("id") cityId: String,
+        @Query(APP_ID_KEY) appId: String = APP_ID_VALUE
+    ): Single<ForecastsResponse>
 }
+
+
